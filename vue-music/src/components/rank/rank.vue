d2be23f89b877cab4e56569117a4e319dbbcf0a1
Follish-Max <827864735@qq.com>
2019-04-01 01:45:26 +0800

    vue-music打卡

---
+++ vue-music/src/components/rank/rank.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="rank" ref="rank">
+   <h3>hello</h3>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ vue-music/src/components/singer/singer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="singer" ref="singer">
+    <h3>hello</h3>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
